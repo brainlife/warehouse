3f609e5c13adb09fb01ddb99d75de4031a804621
Soichi Hayashi <hayashis@iu.edu>
2017-05-12 18:48:44 -0400

    adding forgotten component

---
+++ ui/src/components/statusicon.vue
@@ -0,0 +1,15 @@
+<template>
+<span>
+    <icon v-if="status == 'finished'" :scale="scale" name="check"></icon>
+    <icon v-if="status == 'running'" :scale="scale" name="cog" spin></icon>
+    <icon v-if="status == 'requested'" :scale="scale" name="hourglass-o"></icon>
+    <icon v-if="status == 'failed'" :scale="scale" name="exclamation"></icon>
+    <icon v-if="status == 'removed'" :scale="scale" name="trash"></icon>
+</span>
+</template>
+
+<script>
+export default {
+    props: [ 'status', 'scale' ],
+}
+</script>
