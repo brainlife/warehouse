652eb0759bf296e863bed4f1b4f37eb99d603e49
stevengeeky <stevengeeky@gmail.com>
2017-06-27 19:01:54 -0400

    fixed merge issue

--- ui/src/components/select2.vue
+++ ui/src/components/select2.vue
@@ -38,6 +38,7 @@ export default {
             tags: true,
             theme: 'classic'
         };
+        
         if (this.dataAdapter) {
             let self = this;
             
@@ -95,68 +96,5 @@ box-sizing: border-box;
 <style>
 .select2-dropdown {
 z-index: 9900;
-}
-</style>
-<template>
-<select multiple="true">
-    <slot></slot>
-</select>
-</template>
-
-<script>
-import Vue from 'vue'
-export default {
-    props: ['options', 'value'],
-
-    /*
-    data() {
-        return {
-            config: Vue.config,
-        }
-    },
-    */
-
-    mounted: function() {
-        var vm = this;
-        //init select2
-        $(this.$el).select2({
-            data: this.options, 
-            tags: true, 
-            theme: 'classic',
-            //matcher: function(term, text) { console.log("test.........", term, text); return false; },
-        })
-        .val(this.value)
-        .trigger('change')
-        // emit event on change.
-        .on('change',function(evt) {
-            console.dir($(vm.$el).val());
-            vm.$emit('input', $(vm.$el).val());
-        })
-    },
-
-    //watch for parent value/options change and apply
-    watch: {
-        options: function (options) {
-            $(this.$el).select2({ data: options })
-        },
-        value: function(value) {
-            //check to make sure we aren't updateing controller with the same value
-            //this happens if user change value on UI, which triggers change, and parent
-            //send change event back.
-            if(JSON.stringify(value) !== JSON.stringify(this.value)) $(this.$el).val(value).trigger('change');
-        },
-    },
-
-    //clean up
-    destroyed: function () {
-        $(this.$el).off().select2('destroy')
-    },
-}
-</script>
-
-<style scoped>
-select {
-width: 100%;
-box-sizing: border-box;
 }
 </style>
