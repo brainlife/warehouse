e89246e51df5cabe78411202478774b5b57f036b
Soichi Hayashi <hayashis@iu.edu>
2017-07-28 21:51:06 +0000

    added pm2 start for new services

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,7 @@
 #This script is used inside the docker container to start api and ui(via http-server)
 
 pm2 start -i 4 /app/api/warehouse.js
-#pm2 start http-server --name ui -- -p 80 -a 0.0.0.0 -d false /app/ui/dist
+pm2 start /app/bin/rule_handler.js
+pm2 start /app/bin/event_handler.js
 
 pm2 logs
