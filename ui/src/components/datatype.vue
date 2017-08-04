617605878f88ba7b8160f6d90849d9360a3bc488
Soichi Hayashi <hayashis@iu.edu>
2017-08-04 18:16:55 -0400

    minor style updates for datatype component

--- ui/src/components/datatype.vue
+++ ui/src/components/datatype.vue
@@ -12,13 +12,16 @@
         </el-row>
     </div>
 
-    <small>Files</small>
+    <!--<small>Files</small>-->
     <el-row :gutter="20">
     <el-col :span="6" v-for="file in datatype.files" :key="file.id">
-        <el-card class="file">
-            <span v-if="file.filename"><icon name="file-o"></icon> <b>{{file.id}}</b> <small class="text-muted">{{file.filename}}</small></span>
-            <span v-if="file.dirname"><icon name="folder"></icon> <b>{{file.id}}</b> <small class="text-muted">{{file.dirname}}</small></span>
-        </el-card>
+        <div style="margin-bottom: 8px;">
+            <span v-if="file.filename"><icon name="file-o"></icon> <b>{{file.id}}</b> 
+                    <small class="text-muted"><code>{{file.filename}}</code></small></span>
+            <span v-if="file.dirname"><icon name="folder"></icon> <b>{{file.id}}</b> 
+                    <small class="text-muted"><code>{{file.dirname}}</code></small>
+            </span>
+        </div>
     </el-col>
     </el-row>
 
