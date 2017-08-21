77d53ac2b8ab9a76f693755bb1708fb87d3b7835
stevengeeky <stevengeeky@gmail.com>
2017-08-21 11:14:41 -0400

    setup for config editor

---
+++ ui/src/components/configedit.vue
@@ -0,0 +1,18 @@
+<template>
+<div ref="network">
+    <!-- TODO -->
+</div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import vis from 'vis'
+
+export default {
+    mounted () {
+        console.log("TODO..");
+    }
+}
+
+</script>
