10dfa50be6ec8c41fcf6aafbfaf1eabcf6b28bba
Soichi Hayashi <hayashis@iu.edu>
2017-12-18 10:30:30 -0500

    update to reload datasets list when user remove / upload new dataset. tidied up a few things on dataset modal

--- ui/src/components/taskconfig.vue
+++ ui/src/components/taskconfig.vue
@@ -1,8 +1,12 @@
 <template>
-<div>
+<div v-if="taskconfig">
+    <span class="text-muted" v-if="Object.keys(taskconfig).length == 0">No configuration</span>
     <b-row v-for="(v,k) in taskconfig" :key="k">
         <b-col :cols="4"><span class="text-muted">{{k}}</span></b-col>
-        <b-col><b>{{v}}</b></b-col>
+        <b-col v-if="typeof v == 'object'">
+            <pre v-highlightjs style="margin-bottom: 0px"><code class="json hljs">{{v}}</code></pre>
+        </b-col>
+        <b-col v-else><b>{{v}}</b></b-col>
     </b-row>
     <!--<pre v-if="config.debug">{{task.config}}</pre>-->
 </div>
@@ -13,19 +17,20 @@ import Vue from 'vue'
 
 export default {
     props: {
-        taskid: {type: String},
+        //one of the following needs to be set
+        task: Object,
+        taskid: String,
     },
     data() {
         return {
             //task: null,    
-
             taskconfig: null,
 
             config: Vue.config,
         }
     },
     mounted: function() {
-        console.log("mounted", this.taskid);
+        //console.log("mounted taskconfig");
         this.load();
     },
 
@@ -33,26 +38,36 @@ export default {
         taskid: function() {
             this.load();
         },
+        task: function() {
+            console.log("task changed");
+            this.load();
+        },
     },
 
     methods: {
         load: function() {
-            if(!this.taskid) return;
-            this.$http.get(Vue.config.wf_api+'/task/'+this.taskid).then(res=>{
-                let task = res.body;
-
-                //create key/value of scalar config
-                this.taskconfig = {};
-                for(let id in task.config) {
-                    if(id[0] == "_") continue;
-                    let v = task.config[id];
-                    let vs = v.toString();
-                    if(vs.indexOf("..") != 0) this.taskconfig[id] = v;
-                }
-
-                //create key(input id)/dataset_id of _input
-            });
+            if(this.task) {
+                this.load_config(this.task.config);
+            } else {
+                if(!this.taskid) return;
+                this.$http.get(Vue.config.wf_api+'/task/'+this.taskid).then(res=>{
+                    this.load_config(res.body.config);
+                    //create key(input id)/dataset_id of _input
+                });
+            }
         },
+
+        load_config: function(config) {
+            //create key/value of scalar config
+            this.taskconfig = {};
+            for(let id in config) {
+                if(id[0] == "_") continue;
+                let v = config[id];
+                let vs = v.toString();
+                if(vs.indexOf("..") != 0) this.taskconfig[id] = v;
+            }
+
+        }
     },
 
 }
