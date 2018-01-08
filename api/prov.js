bc004816faba4d4e0fdb074d4cfb422cd6614471
Soichi Hayashi <hayashis@iu.edu>
2018-01-08 14:50:12 +0000

    fixed the issue with doi registration on production datacite API endpoint made neo4j registration optional.

--- api/prov.js
+++ api/prov.js
@@ -9,20 +9,25 @@ const neo4j = require('neo4j');
 const config = require('./config');
 const logger = new winston.Logger(config.logger.winston);
 
-const db = new neo4j.GraphDatabase(config.neo4j);
-
-//test
-logger.debug("testing connection to neo4j");
-db.cypher({
-    //query: 'MATCH (t:Task)-[r]-(src) RETURN t,r,src LIMIT 100',
-    query: 'MATCH (n) RETURN count(*)',
-}, (err, results)=>{
-    if(err) throw err;
-    console.log("neo4j node count", JSON.stringify(results[0], null, 4));
-});
+let db = null;
+if(config.neo4j) {
+    db = new neo4j.GraphDatabase(config.neo4j);
+   
+    //test
+    logger.debug("testing connection to neo4j");
+    db.cypher({
+        //query: 'MATCH (t:Task)-[r]-(src) RETURN t,r,src LIMIT 100',
+        query: 'MATCH (n) RETURN count(*)',
+    }, (err, results)=>{
+        if(err) throw err;
+        console.log("neo4j node count", JSON.stringify(results[0], null, 4));
+    });
+}
 
 //make sure the task and relationship to parent tasks exists
 exports.register_task = (task, cb)=>{
+    if(!db) return cb();
+    
     //console.log(JSON.stringify(task, null, 4));
 
     if(!task.config) return; //noop?
@@ -64,6 +69,8 @@ exports.register_task = (task, cb)=>{
 }
 
 exports.register_dataset = (dataset, cb)=>{
+    if(!db) return cb();
+    
     //dataset
     var query = `MERGE (d:Dataset {dataset_id: '${dataset._id.toString()}'`
     if(dataset.prov.subdir) query += `,subdir: '${dataset.prov.subdir}'`;
@@ -83,6 +90,8 @@ exports.register_dataset = (dataset, cb)=>{
 }
 
 exports.query = (dataset, cb)=>{
+    if(!db) return cb();
+
     var query = `MATCH (d:Dataset{dataset_id: '${dataset._id}'}) MATCH (d)-[*]->(n) MATCH (s)-[r]->(n) RETURN s,r,n`;
     logger.debug(query);
     db.cypher({ query }, cb);
