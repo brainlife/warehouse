4b04b714f9c9e6053865f877bbf1935ecd30f0e5
stevengeeky <stevengeeky@gmail.com>
2018-07-03 13:06:36 +0000

    added support for _order on configform

--- ui/src/components/configform.vue
+++ ui/src/components/configform.vue
@@ -1,29 +1,29 @@
 <template>
 <div>
-    <b-row v-for="(v,k) in spec" :key="k" v-if="v.type && v.type != 'input'">
-        <b-col cols="3" class="text-muted">{{k}} <span v-if="!v.optional">*</span></b-col>
+    <b-row v-for="(v,idx) in sorted_spec" :key="idx" v-if="v.type && v.type != 'input'">
+        <b-col cols="3" class="text-muted">{{v.id}} <span v-if="!v.optional">*</span></b-col>
         <b-col>
             <b-form-group>
                 <!--integer will be deprecated (still used..)-->
                 <b-form-input type="number" v-if="v.type == 'number' || v.type == 'integer'" @mousewheel.native="handle_scroll"
                     :min="v.min" :max="v.max" :step="0.001" :readonly="v.readonly" :required="!v.optional"
-                    v-model.number="value[k]" :placeholder="v.placeholder"/>
+                    v-model.number="value[v.id]" :placeholder="v.placeholder"/>
 
                 <!--string-->
                 <b-form-input type="text" v-if="v.type == 'string'"
                     :readonly="v.readonly" :required="!v.optional"
-                    v-model="value[k]" :placeholder="v.placeholder"/>
+                    v-model="value[v.id]" :placeholder="v.placeholder"/>
 
                 <!--boolean-->
                 <div v-if="v.type == 'boolean'">
                     <b-form-checkbox :disabled="v.readonly"
-                    v-model="value[k]">{{v.desc}}</b-form-checkbox>
+                    v-model="value[v.id]">{{v.desc}}</b-form-checkbox>
                 </div>
 
                 <!--select-->
                 <b-form-select v-if="v.type == 'enum'" 
                     :disabled="v.readonly" :required="!v.optional" 
-                    v-model="value[k]" :placeholder="v.placeholder">
+                    v-model="value[v.id]" :placeholder="v.placeholder">
                     <option :value="null" v-if="v.optional">(don't specify)</option>
                     <option v-for="(option, idx) in v.options" :key="idx" :value="option.value">
                         {{option.label}} <small>({{option.value}})</small>
@@ -63,6 +63,25 @@ export default {
             }
         }
     },
+    computed: {
+        sorted_spec: function() {
+            let spec_arr = [];
+            for (let k in this.spec) {
+                spec_arr.push(Object.assign({}, this.spec[k], { id: k }));
+            }
+            
+            spec_arr.sort((a, b) => {
+                if (typeof a._order != 'number' || typeof b._order != 'number') {
+                    return 0;
+                }
+                return a._order > b._order ? 1 : -1;
+            });
+            
+            console.log(spec_arr);
+            return spec_arr;
+        },
+    },
+    
     methods: {
         //prevent vue-form-input's value from getting changed accidentally by use mouse-wheeling on top of focused
         //number input
