930417044136310a22423e304a7b87a49324f473
Soichi Hayashi <hayashis@iu.edu>
2018-10-31 13:41:29 -0400

    refactored modal list refactored task waiter

---
+++ ui/src/components/modals.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <!--modal that can be called from any view -->
+    <dataset/>
+    <datatype/>
+    <agreements/>
+    <downscript/>
+
+    <!--models only used by project page (TODO - move to project page eventually -->
+    <div v-if="config.user">
+        <viewselecter/>
+        <datasetselecter/>
+        <uploader/>
+        <instanceselecter/>
+        <archiver/>
+    </div>
+</div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+
+import viewselecter from '@/modals/viewselecter'
+import dataset from '@/modals/dataset'
+import datatype from '@/modals/datatype'
+import datasetselecter from '@/modals/datasetselecter'
+import uploader from '@/modals/uploader'
+import instanceselecter from '@/modals/instanceselecter'
+import archiver from '@/modals/archiver'
+import agreements from '@/modals/agreements'
+import downscript from '@/modals/downscript'
+
+export default {
+    components: {
+        viewselecter, dataset, datatype, 
+        uploader, datasetselecter, instanceselecter, 
+        archiver, agreements, downscript,
+    },
+    data() {
+        return {
+            config: Vue.config,
+        }
+    },
+}
+
+</script>
+
+
---
+++ ui/src/mixins/wait.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import Vue from 'vue'
+
+export default {
+    data: function() {
+        return {
+            task: null, 
+            ws: null,
+        }
+    },
+    destroyed() {
+        console.log("mixin wait destroyed");
+    },
+    mounted() {
+        console.log("mixin mounted");
+    },
+
+    methods: {
+        wait: function(taskid, cb) {
+            console.log("waiting for taskid", taskid);
+            this.$http.get(Vue.config.wf_api+'/task', {params: {
+                find: JSON.stringify({ _id: taskid, })
+            }}).then(res=>{
+                this.task = res.body.tasks[0];
+                if(!this.task) console.error("no such task", taskid);
+                switch(this.task.status) {
+                case 'finished':
+                    cb(this.task); 
+                    break;
+                case 'removed':
+                    console.debug("rerunning");
+                    this.$http.put(Vue.config.wf_api+'/task/rerun/'+this.taskid).then(res => {
+                        console.log("rerunning task");
+                        setTimeout(()=>{this.wait(taskid, cb)}, 300);
+                    });
+                    break;
+                default:
+                    setTimeout(()=>{this.wait(taskid, cb)}, 300);
+                }
+            });
+        },  
+        rerun() {
+        },
+    }
+}
+
+
