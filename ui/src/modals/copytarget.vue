17b19ddf05f2aee89dbb24475b049524d3da50f9
Soichi Hayashi <hayashis@iu.edu>
2019-01-11 17:04:41 -0500

    added copytarget modal

---
+++ ui/src/modals/copytarget.vue
@@ -0,0 +1,143 @@
+<template>
+<b-modal :no-close-on-backdrop='true' title="Copy Datasets" ref="modal" size="lg" @ok="submit">
+    <p class="text-muted">Please select project where you want to copy selected datasets to</p>
+    <br>
+    <b-row>
+        <b-col :cols="3">Project</b-col>
+        <b-col>
+            <projectselecter v-if="shown" canwrite="true" v-model="project" :required="true"/>
+            <!--<small>Project to run a new process</small>-->
+        </b-col>
+    </b-row>
+    <br>
+    <!--
+    <b-row>
+        <b-col :cols="3">Process</b-col>
+        <b-col>
+            <b-form-radio-group v-model="createnew" style="margin-bottom: 5px;">
+                <b-form-radio :value="true">Create New Process</b-form-radio>
+                <b-form-radio :value="false" v-if="instances.length > 0">Use Existing Process</b-form-radio>
+            </b-form-radio-group>
+            <b-form-input v-if="createnew" type="text" v-model="desc" placeholder="Enter Description for New Process"/>
+            <v-select v-if="!createnew" v-model="instance" :options="instances" label="_label" required placeholder="(Choose Process)"/>
+        </b-col>
+    </b-row>
+    <br>
+    -->
+    <div slot="modal-footer">
+        <b-button variant="primary" v-if="isvalid()" @click="submit">Submit</b-button>
+    </div>
+</b-modal>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import projectselecter from '@/components/projectselecter'
+
+export default {
+    components: { projectselecter },
+
+    data() {
+        return {
+            submit_cb: null, 
+            shown: false,
+            project: null, 
+            //instance: null, 
+            //desc: "",
+
+            //options
+            projects: {},
+            //instances: [],
+
+            //createnew: true, //for toggle button
+        }
+    },
+
+    destroyed() {
+        this.$root.$off("copytarget.open");
+    },
+
+    watch: {
+        project: function() {
+            /*
+            //refresh instance list
+            this.instances = [];
+            if(!this.project) return;
+            if(!this.projects) return;
+            var project = this.projects[this.project];
+            if(!project) {
+                console.error("can't find", this.project, "in project list", this.projects);
+                return;
+            }
+            this.$http.get(Vue.config.wf_api+'/instance', {params: {
+                find: JSON.stringify({
+                    "config.brainlife": true,
+                    status: {$ne: "removed"},
+                    group_id: project.group_id,
+                    "config.removing": {$exists: false},
+                }),
+                limit: 1000,
+                sort: '-create_date',
+            }}).then(res=>{
+                res.body.instances.forEach(instance=>{
+                    instance._label = instance.desc||'('+instance._id+')';
+                    this.instances.push(instance);
+                });
+                this.instance = this.instances[0];//might be empty
+            });
+            */
+        },
+    },
+
+    mounted() {
+        this.$root.$on("copytarget.open", cb=>{
+            this.submit_cb = cb;
+            this.$refs.modal.show()
+            this.shown = true;
+            //this.instance = null;
+            //this.desc = "";
+        });
+
+        this.$http.get('project', {params: {
+            find: JSON.stringify({ 
+                $or: [
+                  {members: Vue.config.user.sub},
+                  {admins: Vue.config.user.sub},
+                ],
+                removed: false,
+            }),
+            limit: 500,
+            select: 'group_id',
+        }})
+        .then(res=>{
+            this.projects = {};
+            res.body.projects.forEach((p)=>{
+                this.projects[p._id] = p;
+            });
+        });
+    },
+
+    methods: {
+        isvalid: function() {
+            if(!this.project) return false;
+            return true;
+        },
+
+        submit: function() {
+            this.submit_cb({
+                project_id: this.project,
+                //group_id: this.projects[this.project].group_id,
+                //instance: (this.createnew?null:this.instance),
+                //desc: this.desc.trim(),
+            });
+            this.$refs.modal.hide()
+            this.shown = false;
+        },
+        cancel: function() {
+            this.$refs.modal.hide();
+            this.shown = false;
+        },
+    },
+} 
+</script>
