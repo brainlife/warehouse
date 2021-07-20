2cba546fdd74c47adef9ad9017b4fcca1e27fe44
Dheeraj Bhatia <dheerajb@buffalo.edu>
2021-07-20 19:46:57 +0000

    added collapsible and shows all data

--- ui/src/components/user.vue
+++ ui/src/components/user.vue
@@ -1,8 +1,34 @@
 <template>
     <div v-if="user">
         <b-card>
-            <h6>{{user.username}}</h6>
-            <h5>{{user.fullname}}</h5>
+            <div style="float: right;top: 0px">
+                <icon name="edit" scale="1.25"/>
+                  <b-button v-b-toggle="''+user._id">Show More</b-button>
+            </div>
+            <h5>{{user.username}}</h5>
+            <h6>{{user.fullname}} | {{user.email}} <b-badge v-if="user.email_confirmed" variant="success">Confirmed</b-badge> <b-badge v-else variant="danger">Not Confirmed</b-badge></h6>
+        
+            <b-collapse v-bind:id="user._id" class="mt-2">
+                <b-card>
+                    <b-container class="bv-example-row">
+                        <b-row>
+                            <b-col>
+                                <h6>User Profile</h6>
+                                <pre>{{user.profile}}</pre>
+                                <br>
+                                <h6>Scopes</h6>
+                                <pre>{{user.scopes}}}</pre>
+                            </b-col>
+                            <b-col>
+                                <h6>Timestamps</h6>
+                                <pre>{{user.times}}</pre>
+                                <h6>Ext</h6>
+                                <pre>{{user.ext}}</pre>
+                            </b-col>
+                        </b-row>
+                    </b-container>
+                </b-card>
+            </b-collapse>
         </b-card>    
     </div>
 </template>
@@ -15,3 +41,9 @@ export default {
     }
 }
 </script>
+<style scoped>
+pre {
+    background: #ececec;
+    padding : 15px;
+}
+</style>
