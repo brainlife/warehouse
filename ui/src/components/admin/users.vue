e3e6e799e5232795e9c28c7f02a9fb41dc21f34d
Dheeraj <dheerajb@buffalo.edu>
2022-01-20 17:39:11 -0500

    removed comment

--- ui/src/components/admin/users.vue
+++ ui/src/components/admin/users.vue
@@ -302,7 +302,6 @@ export default {
             this.load();
         },
         unlockAccount() {
-            //this.form.ext.openids[0] = this.openids;
             this.$http.post(Vue.config.auth_api+"/local/unlockuser", {email: this.form.email}).then(res=>{
                 this.$notify({type: "success", text: res.data.message});
                 this.closeModal();
