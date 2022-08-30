55ab4039f075ba74d9302320fd0c3f0425bf8fac
Soichi Hayashi <soichih@gmail.com>
2022-08-30 12:26:31 -0400

    fixed reset_health bin

--- bin/reset_health.js
+++ bin/reset_health.js
@@ -6,19 +6,16 @@ const config = require('../api/config');
 const db = require('../api/models');
 const common = require('../api/common');
 
-common.connectRedis(err=>{
+common.connectRedis(async err=>{
     console.log("removing health.warehouse.*");
-    common.redisClient.keys("health.warehouse.*", (err, keys)=>{
-        if(err) throw err;
-        if(keys.length == 0) {
-            console.log("no keys to remove");
-            r.quit();
-        }
-        common.redisClient.del(keys, (err, reps)=>{
-            if(err) throw err;
-            console.log("removed values");
-            console.log(reps);
-            r.quit();
-        });
-    });
+    const keys = await common.redisClient.keys("health.warehouse.*");
+    if(keys.length == 0) {
+        console.log("no keys to remove");
+    } else {
+        const rep = await common.redisClient.del(keys);
+        console.log("removed values");
+        console.debug(rep);
+    }
+    process.exit(0);
+    
 });
