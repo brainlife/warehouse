3a21bb971814ecd603a6e9f0734b1e0a9b47c767
Anibal <anibalsolon@gmail.com>
2022-10-11 19:49:29 -0500

    enh: adjust layout

--- ui/src/resourceedit.vue
+++ ui/src/resourceedit.vue
@@ -8,23 +8,23 @@
                 <b-tab title="Apps">
                     <template v-slot:title>
                         Apps
-                        <span style="opacity: 0.6; font-size: 80%">{{resource.config.services.length}}</span>
+                        <span class="resouce-apps-count__span">{{resource.config.services.length}}</span>
                     </template>
                 </b-tab>
             </b-tabs>
         </b-container>
     </div>
-    <b-form class="page-content">
-        <b-container>
-            <br>
-
-            <div v-if="tab == 0">
-                <div style="padding-bottom: 10px">
-                    <b-form-checkbox v-model="resource.active">Active</b-form-checkbox>
-                    <p>
-                        <small>Uncheck this to temporarily disable this resource.</small>
-                    </p>
-                </div>
+    <b-form>
+        <div v-if="tab == 0" class="page-content">
+            <b-container>
+                <b-row>
+                    <b-col cols="12">
+                        <b-form-checkbox v-model="resource.active">Active</b-form-checkbox>
+                        <p>
+                            <small>Uncheck this to temporarily disable this resource.</small>
+                        </p>
+                    </b-col>
+                </b-row>
 
                 <b-row>
                     <b-col cols="3">
@@ -32,7 +32,6 @@
                     </b-col> 
                     <b-col cols="9">
                         <b-input type="text" v-model="resource.name" placeholder="Resource Name"/>
-                        <br>
                     </b-col>
                 </b-row>
 
@@ -43,7 +42,6 @@
                     <b-col cols="9">
                         <p>
                             <contact :id="resource.user_id"/>
-                            <br>
                             <small>Users who registered this resource and administer this resource.</small>
                         </p>
                     </b-col>
@@ -92,8 +90,6 @@
                             <small>Please select projects that you'd like enable this resource. Any jobs submitted by any member of specified project will be executed on this resource. You have to be listed as administrator of the project to be able to select it.</small>
                         </p>
                     </b-col>
-                    <br>
-                    <br>
                 </b-row>
 
                 <b-row>
@@ -102,7 +98,6 @@
                     </b-col> 
                     <b-col cols="9">
                         <b-input type="text" v-model.trim="resource.avatar" placeholder="Avatar URL"/>
-                        <br>
                     </b-col>
                 </b-row>
 
@@ -169,28 +164,27 @@
                     </b-col> 
                     <b-col cols="9">
                         <p>
-                            <b>Public Key</b>
-                            <br>
+                            <b>Public Key</b><br/>
                             <small>The following ssh public key should be stored in ~/.ssh/authorized_keys on this resource.</small>
                             <b-form-textarea :rows="6" v-model="resource.config.ssh_public"/>
                         </p>
                         <p>
-                            <b>Private Key</b>
-                            <br>
+                            <b>Private Key</b><br/>
                             <small>Brainlife will use the following private key to access this resource (You should not have to copy/paste this out of here).</small>
                             <b-form-checkbox v-if="resource.config.enc_ssh_private === true" v-model="resource.config.enc_ssh_private">Use the current private key</b-form-checkbox>
                             <b-form-textarea v-if="resource.config.enc_ssh_private !== true" :rows="3" v-model="resource.config.enc_ssh_private"/>
                         </p>
                         <p>
-                            <b-btn @click="reset_sshkey" size="sm">Issue New Keypair</b-btn>
+                            <b-btn @click="reset_sshkey" size="sm" :disabled="resetting_sshkey">Issue New Keypair</b-btn>
                         </p>
 
                     </b-col>
                 </b-row>
+            </b-container>
+        </div>
 
-            </div><!--end of detail tab-->
-
-            <div v-if="tab == 1">
+        <div v-if="tab == 1" class="page-content">
+            <b-container>
                 <p>
                     <b-input-group prepend="Max Jobs">
                         <b-input type="text" v-model="resource.config.maxtask"/>
@@ -201,37 +195,42 @@
                 <p>
                     <small>The following Apps are allowed to run on this resource.</small>
                 </p>
-                <div v-for="(service, idx) in resource.config.services" :key="idx" style="margin-bottom: 5px;">
-                    <b-button @click="remove_service(service)" size="sm" text="Button" variant="danger" style="float: right;"><icon name="trash"/></b-button>
-                    <div style="padding-right: 50px;">
-                        <b-input-group prepend="Name">
-                            <b-form-input v-model="service.name" list="service_names" trim></b-form-input>
-                            <datalist id="service_names">
-                                <option v-for="service in service_names">{{service}}</option>
-                            </datalist>
-                            <b-input-group-prepend is-text>Score</b-input-group-prepend>
-                            <b-form-input v-model="service.score"></b-form-input>
-                        </b-input-group>
-                    </div>
+                <div v-for="(service, idx) in resource.config.services" :key="idx" class="resource-apps-detail__div">
+                    <b-row>
+                        <b-col>
+                            <b-input-group>
+                                <b-input-group-prepend is-text>Name</b-input-group-prepend>
+                                <b-form-input v-model="service.name" list="service_names" trim></b-form-input>
+                                <datalist id="service_names">
+                                    <option v-for="service in service_names">{{service}}</option>
+                                </datalist>
+                                <b-input-group-prepend is-text>Score</b-input-group-prepend>
+                                <b-form-input v-model="service.score"></b-form-input>
+                            </b-input-group>
+                        </b-col>
+                        <b-col cols="auto">
+                            <b-button @click="remove_service(service)" size="sm" text="Button" variant="danger"><icon name="trash"/></b-button>
+                        </b-col>
+                    </b-row>
                 </div>
                 <p>
                     <b-btn @click="add_service" variant="success" size="sm">Add App</b-btn>
                 </p>
-            </div>
-            <div class="page-footer">
-                <b-container>
-                    <b-button variant="danger" @click="remove" v-if="this.resource._id" style="float: left;"><icon name="trash"/> Remove</b-button>
-                    <b-button variant="secondary" @click="cancel">Cancel</b-button>
-                    <b-button variant="primary" @click="submit" :disabled="submitting"><icon v-if="submitting" name="cog" spin/> Submit</b-button>
-                </b-container>
-            </div>
-                
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-        </b-container>
+            </b-container>
+        </div>
+        <div class="page-footer">
+            <b-container>
+                <b-row no-gutters>
+                    <b-col cols="auto">
+                        <b-button variant="danger" @click="remove" v-if="this.resource._id"><icon name="trash"/> Remove</b-button>
+                    </b-col>
+                    <b-col>
+                        <b-button variant="secondary" @click="cancel">Cancel</b-button>
+                        <b-button variant="primary" @click="submit" :disabled="submitting"><icon v-if="submitting" name="cog" spin/> Submit</b-button>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
         <!--
         <div v-if="config.debug">
             <pre>{{JSON.stringify(resource, null, 4)}}</pre>
@@ -249,8 +248,8 @@ import contactlist from '@/components/contactlist'
 import contact from '@/components/contact'
 import tageditor from '@/components/tageditor'
 
-//let's lazy fetch node-forge (it's pretty big..)
-const getForge = ()=>import('node-forge')
+// Lazy import, given the module is big
+const getForge = () => import('node-forge')
 
 export default {
     components: { 
@@ -262,38 +261,31 @@ export default {
     data () {
         return {
             resource: null,
-
             projects: null,
-
             tab: 0,
-
             envs_: "",
-            //global: false, 
-            //archive_access: false, 
-
-            service_names: [], //all services that user can choose from
-
+            service_names: [], // All services that user can choose from
             submitting: false,
-
+            resetting_sshkey: false,
             config: Vue.config,
         }
     },
 
     mounted: function() {
 
-        //load all apps service names
+        // Load all apps service names
         this.$http.get("app", {params: {
             select: "github",
             limit: 1000,
         }}).then(res=>{
-            this.service_names = res.data.apps.map(app=>app.github);
-            this.service_names = [...new Set(this.service_names)]; //debupe
-            this.service_names = this.service_names.filter(name=>name && name.includes("/")); //remove odd looking service names
+            this.service_names = res.data.apps.map(app => app.github);
+            this.service_names = [...new Set(this.service_names)]; // remove duplicates
+            this.service_names = this.service_names.filter(name => name && name.includes("/")); // remove odd looking service names
             this.service_names.sort();
         });
 
         if(this.$route.params.id !== '_') {
-            //TODO use resource_cache mixin?
+            // TODO use resource_cache mixin?
             this.$http.get(Vue.config.amaretti_api+'/resource', {params: {
                 find: JSON.stringify({_id: this.$route.params.id})
             }}).then(res=>{
@@ -318,12 +310,11 @@ export default {
             this.reset_sshkey();
             this.loadProjects();
         }
-
     },
 
     methods: {
         loadProjects() {
-            //load project that user can share this resource with
+            // Load projects that user can share this resource with
             this.$http.get('project', {params: {
                 find: JSON.stringify({
                     removed: false,
@@ -353,7 +344,7 @@ export default {
                     });
                 }
 
-                //add private groups that user doesn't have access to.. so the value won't disappear
+                // Add groups that user doesn't have access to, so the selection will not disappear
                 this.resource.gids.forEach(gid => {
                     const project = this.projects.find(p => p.group_id == gid);
                     if (!project) {
@@ -387,26 +378,33 @@ export default {
         },
 
         reset_sshkey() {
-            delete this.resource.config.enc_ssh_private;
-            getForge().then(forge=>{
-                //publicKeyToOpenSSH only works for rsa.. not forge.pki.ed25519
-                forge.pki.rsa.generateKeyPair({bits: 2048, workers: 2/*e: 0x10001*/}, (err, keypair)=>{
-                    if(err) {
-                        this.$notify({type: 'error', text: err});
-                        return;
-                    }
-                    Vue.set(this.resource.config, 'ssh_public', forge.ssh.publicKeyToOpenSSH(keypair.publicKey));//, "pubkey comment");
-                    Vue.set(this.resource.config, 'enc_ssh_private', forge.ssh.privateKeyToOpenSSH(keypair.privateKey));
-                });
-            });
+            if (this.resetting_sshkey) return;
+            this.resetting_sshkey = true;
+            getForge()
+                .then(forge => {
+                    forge.pki.rsa.generateKeyPair({bits: 2048, workers: 2 }, (err, keypair) => {
+                        if (err) {
+                            this.$notify({type: 'error', text: err});
+                            this.resetting_sshkey = false;
+                            return;
+                        }
+                        delete this.resource.config.enc_ssh_private;
+                        Vue.set(this.resource.config, 'ssh_public', forge.ssh.publicKeyToOpenSSH(keypair.publicKey));
+                        Vue.set(this.resource.config, 'enc_ssh_private', forge.ssh.privateKeyToOpenSSH(keypair.privateKey));
+                        this.resetting_sshkey = false;
+                    });
+                })
+                .catch(err => {
+                    this.$notify({type: 'error', text: err});
+                    this.resetting_sshkey = false;
+                })
         },
 
-        submit(evt) {
-            evt.preventDefault(); //TODO do I need this?
-            if(this.submitting) return; //prevent double submission..
+        submit() {
+            if (this.submitting) return; // Prevent double submission
             this.submitting = true;
-            //validate things
-            if(this.envs_ && this.envs_.trim()) {
+
+            if (this.envs_ && this.envs_.trim()) {
                 try {
                     this.resource.envs = JSON.parse(this.envs_);
                 } catch(err) {
@@ -415,16 +413,14 @@ export default {
                     return;
                 }
             }
-            if(!this.resource.gids.length) {
+            if (!this.resource.gids.length) {
                 this.$notify({type: 'error', text: "Please specify at least 1 project to use this resource"});
                 this.submitting = false;
                 return;
             }
 
-            if(this.resource._id) {
-                //update
+            if (this.resource._id) { // Update resource
                 this.$http.put(Vue.config.amaretti_api+'/resource/'+this.resource._id, this.resource).then(res=>{
-                    //this.$router.push('/resource/'+this.resource._id);
                     this.$router.go(-1);
                     this.submitting = false;
                 }).catch(err=>{
@@ -432,8 +428,7 @@ export default {
                     console.error(err);
                     this.submitting = false;
                 });
-            } else {
-                //create
+            } else { // Create resource
                 delete this.resource._id;
                 this.$http.post(Vue.config.amaretti_api+'/resource', this.resource).then(res=>{
                     this.$router.replace('/resource/'+res.data._id);
@@ -473,9 +468,23 @@ export default {
 }
 .page-content {
     margin-top: 40px;
+    padding-bottom: 60px;
+}
+.page-content .container {
+    margin-top: 2em;
+}
+.page-content .container .row {
+    margin-bottom: 1em;
 }
 small {
     opacity: 0.5;
+}
+.resource-apps-detail__div {
+    margin-bottom: 1em;
+}
+.resouce-apps-count__span {
+    opacity: 0.6;
+    font-size: 80%;
 }
 .resource-projects-all_admin-message {
     color: #C00;
