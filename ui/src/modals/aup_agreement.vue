0465b9949a89d760dd143f398f7b7c1f729d8d0d
bhatiadheeraj <dheerajbhatia2010@gmail.com>
2023-08-15 21:13:21 +0000

    removed redundant api calls

--- ui/src/modals/aup_agreement.vue
+++ ui/src/modals/aup_agreement.vue
@@ -1,7 +1,7 @@
 <template>
-    <b-modal ref="modal">
+    <b-modal size="lg" ref="modal">
       <div slot="modal-title">Updated User Agreement</div>
-      <iframe :src="agreementURL" style="width: 100%; height: 400px;"></iframe>
+      <iframe :src="agreementURL" style="width: 100%; height: 600px;"></iframe>
       <template #modal-footer>
         <b-button @click="ok" variant="primary">Accept</b-button>
         <b-button @click="close" variant="secondary">Cancel</b-button>
@@ -17,36 +17,9 @@
       return {
         config: Vue.config,
         agreementURL: "https://brainlife.io/docs/aup/",
-        profile: {
-                public: {
-                    institution: "",
-                    position: "",
-                    bio: "",
-                    //institution map coordinate
-                    showOnMap: false,
-                    lat: "",
-                    lng: "",
-                },
-                private: {
-                    aup: false,
-                    /*
-                    neuroimaging_experience: null,
-                    study_experience: null,
-                    programming_experience: null, 
-                    computing_experience: null, 
-                    */
-                    notification: {
-                       //newsletter_general: false,
-                        process_sound: null
-                    }
-                }
-        },
       };
     },
     mounted() {
-        this.$http.get(Vue.config.auth_api+"/profile").then(res=>{
-            if(res.data.profile) lib.mergeDeep(this.profile, res.data.profile);
-        });
       this.$refs.modal.show();
     },
     methods: {
@@ -54,21 +27,13 @@
         this.$refs.modal.hide();
       },
       ok() {
-        this.profile.private.aup = new Date();
-       
-          this.$http.patch(Vue.config.auth_api+"/profile", {
-                profile: this.profile, 
+            const updatedAup = new Date();       
+            this.$http.patch(Vue.config.auth_api+"/profile", {
+                profile: { private: { aup: updatedAup } }
             }).then(res=>{
-                Vue.config.profile = this.profile;
+                Vue.config.profile.private.aup = updatedAup;
+
                 this.$notify({ type: "success", text: "Updated Details" });
-                // update JWT token
-                this.$http.post(Vue.config.auth_api+'/refresh').then(res=>{
-                localStorage.setItem("jwt", res.data.jwt);
-                this.$notify("refreshed");
-                }).catch(err=>{
-                    console.error(err.response);
-                });
-                
                 this.$refs.modal.hide();
             }).catch(err=>{
                 console.error(err.response);
