400012dcbfb0835270e467c5fde3cd1bdcea6825
Anibal Solon <anibalsolon@gmail.com>
2024-01-10 17:28:42 +0000

    enh: cancel request once search text changed

--- ui/src/mixins/searchapp.js
+++ ui/src/mixins/searchapp.js
@@ -7,6 +7,7 @@ export default {
         return {
             search_apps: [],
             search_apps_ignore: [], //list of apps to ignore (set by the client)
+            xhr_request: null,
         }
     },
     methods: {
@@ -23,12 +24,26 @@ export default {
                     _id: {$nin: this.search_apps_ignore },
                     removed: false,
                 };
-                this.$http.get('app', {params: {
-                    find: JSON.stringify(find),
-                    populate: 'inputs.datatype outputs.datatype contributors', //to display app detail
-                    limit: 100, //freesurfer gives a lot of results..
-                }})
+                this.$http.get(
+                    'app',
+                    {
+                        params: {
+                            find: JSON.stringify(find),
+                            populate: 'inputs.datatype outputs.datatype contributors', //to display app detail
+                            limit: 100, //freesurfer gives a lot of results..
+                        },
+                        beforeSend: function(xhr) {
+                            if (this.xhr_request) {
+                                this.xhr_request.abort();
+                            }
+                            this.xhr_request = xhr;
+                        },
+                    }
+                )
                 .then(res=>{
+                    if (res.request !== this.xhr_request) {
+                        return;
+                    }
                     this.search_apps = res.data.apps;
                     loading(false);
                 });
